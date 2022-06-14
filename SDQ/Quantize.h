6e2a5b22d583c30c76de5bfb158c082cc7c7385e
deponce <yedeponce@gmail.com>
2022-06-14 21:35:48 +0800

    fix fake color bug

--- SDQ/Quantize.h
+++ SDQ/Quantize.h
@@ -2,7 +2,6 @@
 #include <iostream>
 using namespace std;
 void Dequantize(double seq_dct_idxs[][64], double Q_table[64], int N_block){
-    cout<<"Dequantize"<<N_block<<endl;
     for(int N=0; N<N_block; N++){
         for(int x=0; x<64; x++){
             seq_dct_idxs[N][x] = seq_dct_idxs[N][x]*Q_table[x];
@@ -13,7 +12,6 @@ void Dequantize(double seq_dct_idxs[][64], double Q_table[64], int N_block){
 
 void Quantize(double seq_dct_coefs[][64], double seq_dct_idxs[][64],
               double Q_table[64],  int N_block){
-    cout<<"Quantize"<<N_block<<endl;
     int N, x;
     for (N=0; N<N_block; N++){
         for(int x=0; x<64; x++){
