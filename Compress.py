0306effabf16d387cea995a5d93b2c9c37b0610f
deponce <yedeponce@gmail.com>
2022-06-17 22:12:24 +0800

    return BPP from dataloader

--- Compress.py
+++ Compress.py
@@ -7,6 +7,34 @@ import HDQ
 import matplotlib.pyplot as plt
 from PIL import Image
 
+class TensorImg(torch.Tensor):
+    _BPP = ''
+    
+    @staticmethod 
+    def __new__(cls, x, BPP, *args, **kwargs): 
+        return super().__new__(cls, x, *args, **kwargs) 
+      
+    def __init__(self, x, BPP): 
+        self._BPP = BPP
+
+    def clone(self, *args, **kwargs): 
+        return TensorImg(super().clone(*args, **kwargs), self._BPP)
+
+    def to(self, *args, **kwargs):
+        new_obj = TensorImg([], self._BPP)
+        tempTensor=super().to(*args, **kwargs)
+        new_obj.data=tempTensor.data
+        new_obj.requires_grad=tempTensor.requires_grad
+        return(new_obj)
+
+    @property
+    def BPP(self):
+        return self._BPP
+        
+    @BPP.setter
+    def BPP(self, _BPP):
+        self._BPP = _BPP
+
 class SDQ_transforms(torch.nn.Module):
     def __init__(self, model="NoModel", Q=50, q=50, J=4, a=4, b=4,
                  Lambda=1, Beta_S=1,Beta_W=1,Beta_X=1,):
@@ -26,7 +54,7 @@ class SDQ_transforms(torch.nn.Module):
         compressed_img, BPP = SDQ.__call__(img, self.model, self.J, self.a, self.b, 
                                            self.Q, self.q, self.Beta_S, self.Lambda, 0.)
         compressed_img = torch.tensor(compressed_img)
-        return compressed_img # {'image': compressed_img, 'BPP': BPP}
+        return{'image': compressed_img, 'BPP': BPP}
 
 class HDQ_transforms(torch.nn.Module):
     def __init__(self, Q=50, q=50, J=4, a=4, b=4):
@@ -40,4 +68,4 @@ class HDQ_transforms(torch.nn.Module):
         compressed_img, BPP = HDQ.__call__(img, self.J, self.a, self.b,
                                            self.Q,self.q)
         compressed_img = torch.tensor(compressed_img)
-        return compressed_img # {'image': compressed_img, 'BPP': BPP}
+        return {'image': compressed_img, 'BPP': BPP}
--- HDQtest.py
+++ HDQtest.py
@@ -22,11 +22,13 @@ Beta_X = 1
 Lmbd = 8
 model = "NoModel"
 eps = 10
+# pretrained_model = models.alexnet(pretrained=True).to(device)
+# _ = pretrained_model.eval()
 device = torch.device('cpu' if torch.cuda.is_available() else 'cpu')
 transform = transforms.Compose([transforms.ToTensor(),
                                 transforms.Normalize(mean=[0, 0, 0], std=[1/255., 1/255., 1/255.]),
                                 HDQ_transforms(QF_Y, QF_C, J, a, b),
-                                transforms.Resize((224, 224))])
+                                ])
 resize = transforms.Resize((224, 224))
 dataset = datasets.ImageNet(root="~/project/data", split='val', transform=transform)
 test_loader = torch.utils.data.DataLoader(dataset, batch_size=Batch_size, shuffle=False, num_workers=6)
@@ -45,32 +47,11 @@ test_loader = torch.utils.data.DataLoader(dataset, batch_size=Batch_size, shuffl
 # plt.show()
 
 normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
-# pretrained_model = models.alexnet(pretrained=True).to(device)
-# _ = pretrained_model.eval()
-# for idx, dt in enumerate(test_loader):
-#     data, tar = dt #[0, 255]
-#     norm_img = resize(normalize(data/255.)).type(torch.FloatTensor).to(device)
-#     output = pretrained_model(norm_img)
-#     init_pred = output.max(1, keepdim=True)[1]
-#     tar = tar.to(device)
-#     if init_pred==tar:
-#         total_counts += 1
-#         data = data[0].numpy()#[0, 255]
-#         compressed_img, bit_rate = SDQ.__call__(data, "Alexnet", J, a, b,
-#                             QF_Y, QF_C, Beta_S, eps)
-#         compressed_img = torch.from_numpy((compressed_img/255.)[None])
-#         compressed_img=compressed_img.type(torch.FloatTensor)
-#         norm_img = resize(normalize(compressed_img)).to(device)
-        
-#         output = pretrained_model(norm_img)
-#         init_pred_comp = output.max(1, keepdim=True)[1]
-#         if init_pred_comp==tar:
-#             corr_counts +=1
-#         print("ACC : ", (corr_counts/(total_counts)*100))
-#         print("Correct : ", corr_counts, "  out ", total_counts)
-# # i = 0
+
 for dt in tqdm.tqdm(test_loader):
     data_BPP, tar = dt
+    resizedimg = resize(data_BPP['image'])
+    print("BPP: ",data_BPP['BPP'])
     # breakpoint()
     # data = data_BPP['image']
     # BPP = data_BPP['BPP']
@@ -84,15 +65,3 @@ for dt in tqdm.tqdm(test_loader):
     # plt.imshow(data_BPP[0].numpy().transpose(1, 2, 0) / 255.)
     # plt.show()
     # print(BPP)
-
-#     break
-# plt.imshow(compressed_img.transpose(1,2,0)/255.)#red
-# plt.show()
-# plt.imshow(data[0]/255.)#red
-# plt.show()
-# plt.imshow(data[1]/255.)#green
-# plt.show()
-# plt.imshow(data[2]/255.)#blue
-# plt.show()
-# plt.imshow(compressed_img.transpose(1,2,0)/255.)
-# plt.show() 
