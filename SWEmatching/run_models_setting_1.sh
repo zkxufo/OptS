3dad5bb614f885306dcc539d482c7e295e9e645a
Ahmed Hussein Salamah <eng.ahmed_hussein@hotmail.com>
2023-06-07 12:41:03 -0700

    last update before adding fast quantization

--- SWEmatching/run_models_setting_1.sh
+++ SWEmatching/run_models_setting_1.sh
@@ -3,10 +3,4 @@
 for model in Squeezenet Shufflenetv2 Regnet Mnasnet mobilenet_v2 Alexnet
 do
     bash setting_1.sh ${model}
-done 
-
-
-for model in Squeezenet Shufflenetv2 Regnet Mnasnet mobilenet_v2 Alexnet
-do
-    bash setting_2.sh ${model}
-done 
+done
